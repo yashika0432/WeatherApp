028668c2ff545984f076a9559ce049efd04c064f
yashika <yashika0432@gmail.com>
2025-11-02 14:05:42 +0530

    searchbox

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -5,6 +5,27 @@ import { useState } from "react";
 
 export default function SearchBox() {
   let [city, setcity] = useState("");
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "784f210d1405df9c2a7d6250aadf1a4a";
+
+  let getweatherinfo = async () => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let Jsonresponse = await response.json();
+    console.log(Jsonresponse);
+
+    let result = {
+      temp: Jsonresponse.main.temp,
+      tempMin: Jsonresponse.main.temp_min,
+      tempMax: Jsonresponse.main.temp_max,
+      humidity: Jsonresponse.main.humidity,
+      feelsLike: Jsonresponse.main.feels_like,
+      weather: Jsonresponse.weather[0].description,
+    };
+    console.log(result);
+  };
+
   let handlechange = (evnt) => {
     setcity(evnt.target.value);
   };
@@ -13,6 +34,7 @@ export default function SearchBox() {
     evnt.preventDefault();
     console.log(city);
     setcity("");
+    getweatherinfo();
   };
 
   return (
